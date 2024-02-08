f9650bb09f190c15bbdf26c54af668f812196f22
Alice Smith <alice@smith.com>
2024-02-08 15:14:20 -0500

    Added bootstrap, CSS, and homepage structure

--- react-dev-env/src/components/NavigationBar.jsx
+++ react-dev-env/src/components/NavigationBar.jsx
@@ -1,22 +1,31 @@
 import NavigationItem from "./NavigationItem";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function NavigationBar() {
   const navigation = [
-    { title: "Home", link: "home" },
-    { title: "Product", link: "product" },
+    { title: "Home", link: "Home" },
+    { title: "Product", link: "Product" },
   ];
+
   return (
-    <nav>
-      <ul>
-        {navigation.map((navItem, index) => (
-          <NavigationItem
-            key={index}
-            link={navItem.link}
-            title={navItem.title}
-          />
-        ))}
-      </ul>
-    </nav>
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav>
+            {navigation.map((navItem, index) => (
+              <NavigationItem
+                key={index}
+                link={navItem.link}
+                title={navItem.title}
+              />
+            ))}
+          </Nav>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
